8cee51f3b03dd5822ecaa43d8b59da1f87d5e6ee
antonietagi <126415907+antonietagi@users.noreply.github.com>
2024-09-25 19:55:56 -0300

    Create media.for

---
+++ media.for
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+    int i;
+    float media,n;
+   
+    for (i=1; i<=10, i++) {
+        printf("Qual a sua média %d", i);
+        scanf("%f", &media);
+        soma += n;
+    }
+        
+     
+    media = soma/10;   
+    printf("A média das suas notas é %f"; &media);
+
+}
