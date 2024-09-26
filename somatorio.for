350e254785721116484d6d2ecada1314be858898
antonietagi <126415907+antonietagi@users.noreply.github.com>
2024-09-25 22:13:50 -0300

    somatorio.for

---
+++ somatorio.for
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int soma=0, n,i;
+    printf("Digite um número:");
+    scanf("%d", &n);
+    
+    for (i=1; i<=n; i++);
+     soma += soma + (5+i*i+2*i+8);
+     
+     printf("O somatório é igual a: %d", soma);
+    
+}
+ 
